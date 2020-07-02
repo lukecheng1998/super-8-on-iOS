fe9f99c5d4e67e98973db337aea8564220e3de6b
lukecheng1998 <lukecheng1998@gmail.com>
2020-07-02 14:40:40 -0700

    initial Commit

---
+++ super-8-ios/super-8-ios/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  super-8-ios
+//
+//  Created by Luke Cheng on 7/2/20.
+//  Copyright © 2020 Luke Cheng. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
